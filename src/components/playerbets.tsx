24cc2b755e6d83cc0e9528ff70f1676112a43f9d
George Lisec <glisec@alliedsolutions.net>
2024-06-10 16:36:39 -0400

    progress on Total:

--- src/components/playerbets.tsx
+++ src/components/playerbets.tsx
@@ -1,10 +1,16 @@
 import { useState } from "react";
 
+//TODO: Need to show what the take home is for each bet, for submit onClick, calcualte what the take home is.
+
 export default function Bet() {
 
     const [betAmount, setBetAmount] = useState<string | number>('');
-    const bet = Number(betAmount)
-    console.log(bet)
+    const bet: number = Number(betAmount)
+    const [totalAmount, setTotalAmount] = useState<number>(0)
+
+    const handleBet = (bet: number) => {
+        setTotalAmount(totalAmount - bet)
+    }
     
 
     return (
@@ -14,9 +20,9 @@ export default function Bet() {
             type="text" value={betAmount} onChange={e => setBetAmount(e.target.value)} className="px-2 py-1 rounded-xl bg-white font-mono"/>
             <button
             type="submit"
-            onClick={() => setBetAmount(bet)}
             className="px-4 py-4 rounded-xl bg-white">Submit</button>
         </form>
+        <h1 className="text-white text-2xl">{totalAmount + bet}</h1>
         </>
     );
 
